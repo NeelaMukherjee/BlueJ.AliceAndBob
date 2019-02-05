04b4dce48a2eee7b04a543b5b2604a89db7a4f6a
Neelanjana Mukherjee <neelanjanamukherjee@zipcoders-MacBook-Pro.local>
2019-02-05 13:42:43 -0500

    Greeting Aliceor Bob

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args ){
+        
+        Scanner name = new Scanner(System.in);
+        System.out.println("Please Enter your Name");
+        
+        String enteredName=name.nextLine();
+        System.out.println ("Hello "+ enteredName);
+        
 
     }
 }
